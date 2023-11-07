56dd5869a786651369a590dbad8bb8a4f9333421
Data Gurtchumelia <130462560+datagurtchumelia@users.noreply.github.com>
2023-11-07 17:42:39 +0400

    Fixed Sql Injection

--- db_uploader.php
+++ db_uploader.php
@@ -13,42 +13,30 @@ if ($mysqli->connect_errno) {
 
 $file = fopen("emails.txt", "r");
 if ($file) {
-    $values = [];
-    while (($line = fgets($file)) !== false) {
-        $line = trim($line);
+    $stmt = $mysqli->prepare("INSERT INTO emails (email, password) VALUES (?, ?)");
+    if ($stmt) {
+        $values = [];
+        while (($line = fgets($file)) !== false) {
+            $line = trim($line);
 
-        // Skip empty lines or lines starting with '#'
-        if (empty($line) || $line[0] === '#') {
-            continue;
+            // Skip empty lines or lines starting with '#'
+            if (empty($line) || $line[0] === '#') {
+                continue;
+            }
+
+            // Split the line into email and password
+            list($email, $password) = explode(":", $line);
+
+            $email = trim($email);
+            $password = trim($password);
+
+            $stmt->bind_param("ss", $email, $password);
+            $stmt->execute();
         }
 
-        // Split the line into email and password
-        list($email, $password) = explode(":", $line);
-
-        $email = trim($email);
-        $password = trim($password);
-
-        // Escape special characters and surround values with single quotes
-        $email = $mysqli->real_escape_string($email);
-        $password = $mysqli->real_escape_string($password);
-
-        $values[] = "('$email', '$password')";
-
-        // Limit the number of values to insert at once
-        if (count($values) >= 1000) {
-            $query = "INSERT INTO emails (email, password) VALUES " . implode(",", $values);
-            $mysqli->query($query);
-            $values = [];
-        }
+        $stmt->close();
+        fclose($file);
     }
-
-    // Insert any remaining values
-    if (count($values) > 0) {
-        $query = "INSERT INTO emails (email, password) VALUES " . implode(",", $values);
-        $mysqli->query($query);
-    }
-
-    fclose($file);
 }
 
 $mysqli->close();
